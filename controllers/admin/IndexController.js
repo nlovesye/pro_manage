8e6aef1fc5d01d3a9510ac1a5d9323a995596aa0
nlovesye <894765963@qq.com>
2019-02-21 15:49:42 +0800

    dev

--- controllers/admin/IndexController.js
+++
@@ -1,9 +0,0 @@
-const _ = async (ctx, next) => {
-    await ctx.render('admin/index', {
-        title: '后台管理'
-    })
-}
-
-module.exports = {
-    _
-}
--- routes/BaseController.js
+++
@@ -1,8 +0,0 @@
-class BaseController {
-    async run (ctx, next) {
-        // ctx.app.a = '123'
-        await next()
-    }
-}
-
-module.exports = new BaseController()
